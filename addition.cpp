5d4796b6f72f07ad83b48641cea93bc38596942f
ombhombe2344 <ombhombe15@gmail.com>
2024-07-30 07:32:16 +0000

    Initial Commit

---
+++ addition.cpp
@@ -0,0 +1,24 @@
+//om bhombe
+//23070123039
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    cout<<"Enter num 1: "<<endl;
+    cin>>a;
+    cout<<"Enter num 2: "<<endl;
+    cin>>b;
+    c = a+b;
+    cout<<"The No. is: "<<c;
+    
+}
+
+/*Enter num 1: 
+45
+Enter num 2: 
+45
+The No. is: 90
+*/
